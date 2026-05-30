2d0ebb14b07051eb47387d097feea0cee3c1d7f3
mr-piristine <sami.piristine@gmail.com>
2026-05-30 14:03:12 +0300

    updated __about__.py

--- src/tv_downloader/__about__.py
+++ src/tv_downloader/__about__.py
@@ -1,9 +1,9 @@
 # tv_downloader/__about__.py
 
 __version__ = "0.1.0"
-__author__ = "Sami Piristine"
+__author__ = "S.P."
 __email__ = "sprstn@gmail.com"
-__license__ = "MIT"
+__license__ = "Creative Commons - CC0 1.0 Universal"
 __summary__ = "A modular, object-oriented historical data downloader for TradingView."
 
 # You can keep your main documentation here as a clean string asset
